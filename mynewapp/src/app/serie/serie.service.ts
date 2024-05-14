c01f7c732ccef1447f5b4d68aab94bc0674e5913
Dav1d10 <d.carol@uniandes.edu.co>
2024-05-13 19:27:58 -0500

    k

--- mynewapp/src/app/serie/serie.service.ts
+++ mynewapp/src/app/serie/serie.service.ts
@@ -1,12 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Serie } from './serie';
 
 @Injectable({providedIn: 'root'})
 export class SerieService {
 
-    private url: string = environment.baseUrl + 'series';
+    private url: string = environment.baseUrl + 'series.json';
 
     constructor(private http: HttpClient) { }
+
+    getSeries(): Observable<Serie[]> {
+        return this.http.get<Serie[]>(this.url);
+    }
 
 }
