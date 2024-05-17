c9d4cf2036dc471930ea27d27c81ae328a37089a
Dav1d10 <d.carol@uniandes.edu.co>
2024-05-17 01:13:46 -0500

    done

--- mynewapp/src/app/serie/serie-list/serie-list.component.ts
+++ mynewapp/src/app/serie/serie-list/serie-list.component.ts
@@ -10,13 +10,29 @@ import { SerieService } from '../serie.service';
 export class SerieListComponent implements OnInit {
 
   series: Array<Serie> = [];
+  averageSeasons: number = 0;
+  selectedSerie?: Serie;
 
   constructor(private serieService: SerieService) { }
 
   getSeries(): void {
     this.serieService.getSeries().subscribe((series) => {
-      this.series = series;
+    this.series = series;
+    this.calculateAverageSeasons();
     });
+  }
+
+  calculateAverageSeasons() {
+    let totalSeasons = 0;
+    for (let serie of this.series) {
+      totalSeasons += serie.seasons;
+    }
+    this.averageSeasons = this.series.length > 0 ? totalSeasons / this.series.length : 0;
+  }
+
+
+  selectSerie(serie: Serie): void {
+    this.selectedSerie = serie;
   }
 
   ngOnInit() {
--- mynewapp/src/app/serie/serie.module.ts
+++ mynewapp/src/app/serie/serie.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SerieComponent } from './serie.component';
 import { SerieListComponent } from './serie-list/serie-list.component';
+import { SerieDetailComponent } from './serie-detail/serie-detail.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
-  exports: [SerieListComponent],
-  declarations: [SerieListComponent]
+  exports: [SerieListComponent, SerieDetailComponent],
+  declarations: [SerieListComponent, SerieDetailComponent]
 })
 export class SerieModule { }
